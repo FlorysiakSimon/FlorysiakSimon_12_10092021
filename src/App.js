7e3b7c9d1227ac5b0b63a17660e91fcaea257805
simon <simon.florysiak@gmail.com>
2021-09-30 17:07:59 +0200

    remove comment

--- src/App.js
+++ src/App.js
@@ -6,17 +6,6 @@ import HomePage from "./pages/HomePage"
 
 
 export default function App() {
-
-  // fetch('./user12.json').then(response => {
-  //  // console.log(response);
-  //   return response.json();
-  // }).then(data => {
-  //   console.log(data);
-  // }).catch(err => {
-  //   console.log("Error Reading data " + err);
-  // });
-      
-
   return (
     <Router>
         <Header />
