da5527459ff4299a0ae27db2a6ebd9786e772807
simon <simon.florysiak@gmail.com>
2021-10-09 01:34:40 +0200

    add errorpage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Header from './components/Header';
 import UserPage from "./pages/UserPage";
 import HomePage from "./pages/HomePage"
-
+import ErrorPage from "./pages/ErrorPage";
 
 export default function App() {
   return (
@@ -12,6 +12,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/user/:id" component={UserPage} />
+          <Route component={ErrorPage}/>
         </Switch>
     </Router>
   );
